9f483ec59abbe6c10b263e4016e66688a8335004
DEBTULYA2005 <debtulyasarkar463@gmail.com>
2025-05-18 21:26:59 +0530

    change settings

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['52.41.36.82','localhost', '127.0.0.1', '*']
 
 SITE_ID = 1
 
