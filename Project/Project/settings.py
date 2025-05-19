b86bb2f1c072c62982b7ff4356afb907e1b6e7e7
DEBTULYA2005 <debtulyasarkar463@gmail.com>
2025-05-19 14:29:02 +0530

    change setting: change staticfile

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -154,9 +154,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
