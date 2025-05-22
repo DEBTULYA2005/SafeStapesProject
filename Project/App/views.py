4dbe13ab151599651aef01d1a253c74fc379a34d
DEBTULYA2005 <debtulyasarkar463@gmail.com>
2025-05-22 14:11:05 +0530

    trying for staticfile

--- Project/App/views.py
+++ Project/App/views.py
@@ -349,9 +349,10 @@ def upload_avatar(request):
         user = User.objects.get(id=user_id)
         user.avatar = request.FILES['avatar']
         user.save()
-        print(user.avatar)
-        return redirect('dashboard')
-    return redirect('dashboard')
+        return HttpResponse(user.avatar)
+        # return redirect('dashboard')
+    # return redirect('dashboard')
+    return HttpResponse("Avatar not uploaded.")
 
 def book_ride(request):
     if request.method == "POST":
