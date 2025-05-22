dfda494cb399ffb4b38676168ee3600b546e658e
DEBTULYA2005 <debtulyasarkar463@gmail.com>
2025-05-22 15:00:37 +0530

    trying for staticfile

--- Project/App/views.py
+++ Project/App/views.py
@@ -23,10 +23,7 @@ from django.views.defaults import page_not_found
 from django.core.management import call_command
 from django.http import HttpResponse
 import os
-import environ
-import settings.env
-# env = environ.Env()
-# environ.Env.read_env(os.path.join(BASE_DIR, 'secrate.env'))
+
 
 def run_migrations(request):
     if os.environ.get("RENDER") == "true":  # Optional safety check
@@ -353,7 +350,7 @@ def upload_avatar(request):
         user = User.objects.get(id=user_id)
         user.avatar = request.FILES['avatar']
         user.save()
-        return HttpResponse(user.avatar.url + "\n" + settings.CLOUDINARY_STORAGE['CLOUD_NAME'])
+        return HttpResponse(user.avatar.url)
         # return redirect('dashboard')
     # return redirect('dashboard')
     return HttpResponse("Avatar not uploaded.")
