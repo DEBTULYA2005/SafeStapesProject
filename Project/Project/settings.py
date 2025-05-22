2681e975f466ab5ff666e1679d7be5858d4d5c41
DEBTULYA2005 <debtulyasarkar463@gmail.com>
2025-05-22 12:34:16 +0530

    trying for staticfile

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, 'secrate.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = [
     'https://safestapesproject.onrender.com' 
